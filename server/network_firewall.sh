f3b9f7cb65eb0a14b0c88751397e0a0773681ae3
Achraf <achraf.zerhouni-abdou@etu.univ-amu.fr>
2024-04-19 10:35:37 +0200

    add context description

---
+++ server/network_firewall.sh
@@ -0,0 +1,3 @@
+#j'ai commencé par activer via l'IHM de Virtual Box les Adapter 2 et 3 pour le serveur. Je leur ai également affecté respectivement les noms "PV1" et "PV2"
+#Le but maintenant est de configurer les plages IP en utilisant l'outil nmtui
+#Personnellement j'ai utilisé l'outil graphique nmtui (en exécutant la commande nmtui j'ai défini les différents paramètres). Or pour présenter ça dans un fichier sh j'ai traduit ces opérations en commandes shell sans passer par l'interface graphique.
